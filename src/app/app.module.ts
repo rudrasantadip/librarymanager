f7ea6e29b24ff13b7a30690c09b7e5f344e7bcbd
rudrasantadip <santadip9@gmail.com>
2023-10-24 17:57:24 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NavigationComponent } from './navigation/navigation.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
 
 
 
@@ -13,6 +16,9 @@ import { NavigationComponent } from './navigation/navigation.component';
   declarations: [
     AppComponent,
     NavigationComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserprofileComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { navbarData } from './nav-data';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent
+{
+  collapsed:boolean=false;
+  navData = navbarData;
 
+  toggleCollapse():void
+  {
+    this.collapsed=!this.collapsed;
+  }
+
+  closeSideNav():void
+  {
+    this.collapsed=false;
+  }
 }
